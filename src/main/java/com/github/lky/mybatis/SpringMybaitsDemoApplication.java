46182bcd34cb6fb9494394ae9de8d67430ae972c
new12 <happynew_12@163.com>
2017-05-07 20:01:25 +0800

    first commit

---
+++ src/main/java/com/github/lky/mybatis/SpringMybaitsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.github.lky.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMybaitsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMybaitsDemoApplication.class, args);
+	}
+}
