04149afb2d4e71e117cb507302d9a7dbeec2eb77
longkeyu <longkeyulky@didichuxing.com>
2017-05-08 11:46:26 +0800

    fix

---
+++ src/main/java/com/github/lky/mybatis/dao/ext/BookDao.java
@@ -0,0 +1,9 @@
+package com.github.lky.mybatis.dao.ext;
+
+import com.github.lky.mybatis.dao.gen.BookMapper;
+
+/**
+ * Created by longkeyu on 2017/5/8.
+ */
+public interface BookDao extends BookMapper {
+}
---
+++ src/main/java/com/github/lky/mybatis/dao/ext/UserDao.java
@@ -0,0 +1,9 @@
+package com.github.lky.mybatis.dao.ext;
+
+import com.github.lky.mybatis.dao.gen.UserMapper;
+
+/**
+ * Created by longkeyu on 2017/5/8.
+ */
+public interface UserDao extends UserMapper {
+}
